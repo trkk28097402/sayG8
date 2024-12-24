d7ab73d88d36a671ee5d5a0a98ed5104f7166e3f
ThomasC926 <thomasc07102@gmail.com>
2024-12-24 16:52:54 +0800

    BGM Complete

---
+++ Assets/Script/AudioManagerLobby.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagerLobby : MonoBehaviour
+{
+    [Header("----------- Audio Source -----------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("----------- Audio Clip -----------")]
+    public AudioClip Lobby_BGM;
+    public AudioClip ClickSound;
+
+    private void Start()
+    {
+        musicSource.clip = Lobby_BGM;
+        musicSource.Play();
+    }
+    public void PlaySoundEffectClassroom(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+
+}
